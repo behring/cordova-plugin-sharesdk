fdef43ca06fe70664c6bb7cb8f8843a2112969e7
behring <zhaolin53636848@126.com>
2017-02-03 13:14:31 +0800

    修改判断客户端安装方法为promise

--- www/sharesdk.js
+++ www/sharesdk.js
@@ -12,4 +12,10 @@ sharesdkExport.isInstallClient = function (clientType, success, fail) {
   cordova.exec(success, fail, 'ShareSDK', 'isInstallClient', [clientType])
 };
 
+sharesdkExport.isInstallClient.promise = function (clientType) {
+  return new Promise(function (resolve, reject) {
+    sharesdkExport.isInstallClient(clientType, resolve, reject)
+  })
+};
+
 module.exports = sharesdkExport;
